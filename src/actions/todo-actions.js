ea0e62f8a9cb50c8a97b0fe040fec8df8fd8105a
John Robinson <john.robinson@banno.com>
2016-09-16 10:09:22 -0400

    second commit

---
+++ src/actions/todo-actions.js
@@ -0,0 +1,51 @@
+'use strict';
+var actions = actions = actions || {};
+console.log('actions', actions);
+//actions.todo? provides new, delete, update
+actions.todo = {
+
+  listeners: {
+    'create_todo': 'createTodo',
+    'delete_todo': 'deleteTodo',
+    'update_todo': 'updateTodo'
+  },
+
+  createTodo: function(event, detail) {
+    const todo = {complete:false, body:detail.body, saving:true};
+    this.push('todos', todo);
+    const index = this.todos.indexOf(todo);
+    this.async(()=> {
+      this.notifyPath(`todos.${index}.saving`, false);
+    }, 1000);
+  },
+
+  deleteTodo: function(event, detail) {
+    const index = this.todos.indexOf(detail);
+    if(index > -1) {
+      const toBeRemoved = this.todos[index];
+      this.notifyPath(`todos.${index}.saving`, true);
+      //fake network action
+      this.async(()=> {
+        this.notifyPath(`todos.${index}.saving`, false);
+        this.splice('todos', index, 1);
+      }, 1000);
+    }
+  },
+
+  updateTodo: function(event, detail) {
+    console.log('updateTodo:', detail);
+    const index = this.todos.indexOf(detail.update);
+    detail.saving = true;
+    this.notifyPath(`todos.${index}.saving`, detail.saving);
+    this.notifyPath(`todos.${index}.${detail.prop}`, detail.value);
+    //push update events into an observable?
+    //observable debounces x ms until deciding to save?
+    //...
+    this.async(()=> {
+      detail.saving = false;
+      this.notifyPath(`todos.${index}.saving`, detail.saving);
+      this.notifyPath(`todos.${index}.${detail.prop}`, detail.value);
+    }, 1000);
+  }
+
+};
